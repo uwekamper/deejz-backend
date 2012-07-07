bfbeacf82b7b289a5095c9d7efc1bf7f8bb94ecf
Uwe Kamper <me@uwekamper.de>
2012-07-07 21:04:17 +0200

    templates

--- deejz/urls.py
+++ deejz/urls.py
@@ -24,4 +24,8 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
+#urlpatterns += staticfiles_urlpatterns()
