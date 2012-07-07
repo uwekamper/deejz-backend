d362261ed1a8727850a610644504b77cc3342cef
Uwe Kamper <me@uwekamper.de>
2012-07-07 20:06:14 +0200

    bla

--- deejz/settings.py
+++ deejz/settings.py
@@ -170,3 +170,12 @@ LOGGING = {
 
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+if os.getenv('SENDGRID_USERNAME') is not None:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = os.getenv('SENDGRID_USERNAME')
+    EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
